63763b6460575be5850398f1afdab9715d193db3
walterrodriguesjr <walterrjr.86@gmail.com>
2024-08-15 21:06:25 -0300

    atualizado cors.php

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://127.0.0.1:3000', 'http://127.0.0.1:8082'],
+    'allowed_origins' => ['http://127.0.0.1:3000', 'http://127.0.0.1:8082', 'http://192.168.0.2:8000'],
 
     'allowed_origins_patterns' => [],
 
